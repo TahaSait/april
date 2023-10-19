4824fdf77643f475426908b5b3684de2d986d9f2
TahaSait <148285176+TahaSait@users.noreply.github.com>
2023-10-19 15:43:14 -0600

    step 2 corrected

--- temp.py
+++ temp.py
@@ -1,6 +1,7 @@
 # program to convert celsius to Fahrenheit
 def celsius_to_fahrenheit(cel):
-    fah = 25 * 9/5 + 32
+    fah = cel * 9/5 + 32
     return fah
+
 cel = 25
 print(f"25 degress is {celsius_to_fahrenheit(cel)} Fahrenheit")
